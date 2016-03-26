6e672d9972214908129a5bb0bd0b690acf3e2668
Lucas Garron <lgarron@chromium.org>
2016-03-26 02:16:16 -0700

    Implement an `hstspreload` binary. Addresses #25.

---
+++ cmd/hstspreload/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/chromium/hstspreload"
+)
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 2 {
+		fmt.Printf(`hstspreload is a tool for checking conditions to be added to Chromium 's
+HSTS preload list. See hstspreload.appspot.com for more details.
+
+Usage:
+
+  hstspreload command argument
+
+The commands are:
+
+  header    Check an HSTS header for preload requirements
+  url       Check the headers of a URL for preload requirements.
+            Both http:// and https:// URLs can be checked, so the
+            scheme must be specified.
+  domain    Check the TLS configuration and headers of a domain for
+            preload requirements.
+
+Examples:
+
+  hstspreload header "max-age=10886400; includeSubDomains; preload"
+  hstspreload url http://localhost:8080
+  hstspreload domain wikipedia.org
+
+Return code:
+
+  0    Passed all checks.
+  1    Error (failed at least one requirement).
+  2    Had warnings, but passed all requirements.
+  3    Invalid commandline arguments
+  4    Displayed help
+
+`)
+		os.Exit(4)
+		return
+	}
+
+	var issues hstspreload.Issues
+
+	switch args[0] {
+	case "header":
+		issues = hstspreload.CheckHeaderString(args[1])
+
+	case "url":
+		if !strings.HasPrefix(args[1], "http") {
+			fmt.Fprintf(os.Stderr,
+				"Invalid argument: Please supply a scheme (http:// or https://) for the URL.\n")
+			os.Exit(3)
+		}
+
+		response, err := http.Get(args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not connect to URL: %s\n", args[1])
+			os.Exit(3)
+		}
+		issues = hstspreload.CheckResponse(response)
+
+	case "domain":
+		if strings.HasPrefix(args[1], "http") {
+			fmt.Fprintf(os.Stderr,
+				"Invalid argument: Please do not supply a scheme (http:// or https://) before the domain.\n")
+			os.Exit(3)
+		}
+		issues = hstspreload.CheckDomain(args[1])
+	}
+
+	// TODO: Show the HSTS header sent by the domain.
+
+	// Wrap this in a function to (statically) enforce a return code.
+	showResult := func() int {
+		switch {
+		case len(issues.Errors) > 0:
+			fmt.Printf("There were was at least one error.\n\n%v\n", issues)
+			return 1
+
+		case len(issues.Warnings) > 0:
+			fmt.Printf("There were no errors, but at least one warning.\n\n%v\n", issues)
+			return 2
+
+		default:
+			fmt.Printf("Satisfies requirements for preloading.\n")
+			return 0
+		}
+	}
+
+	os.Exit(showResult())
+}
---
+++ doc.go
@@ -0,0 +1,11 @@
+// The hstspreload package has 4 parts:
+//
+// - A Go package with functions to check HSTS preload requirements.
+//
+// - The `hstspreload` command line tool, which can be installed with:
+//     go get github.com/chromium/hstspreload/cmd/hstspreload
+//
+// - Source code for hstspreload.appspot.com
+//
+// - The `transport_security_state_static_generate.go` script.
+package hstspreload

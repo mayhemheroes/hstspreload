910e49904c6d353618972d2cebe7b0b35fdd0bb3
Lucas Garron <lgarron@chromium.org>
2016-04-26 01:05:39 -0700

    Consolidate response tests into a single function.

--- response_test.go
+++ response_test.go
@@ -6,6 +6,23 @@ import (
 	"testing"
 )
 
+const (
+	headerStringsShouldBeEqual = `Did not receive expected header.
+			Actual: "%v"
+			Expected: "%v"`
+)
+
+/******** Examples. ********/
+
+func ExamplePreloadableResponse() {
+	resp, err := http.Get("localhost:8080")
+	if err != nil {
+		header, issues := PreloadableResponse(resp)
+		fmt.Printf("Header: %s", *header)
+		fmt.Printf("Issues: %v", issues)
+	}
+}
+
 /******** Helper functions tests. ********/
 
 func expectNil(t *testing.T, actual *string) {
@@ -23,39 +40,48 @@ func expectString(t *testing.T, actual *string, expected string) {
 
 /******** Response tests. ********/
 
-func ExamplePreloadableResponse() {
-	resp, err := http.Get("localhost:8080")
-	if err != nil {
-		header, issues := PreloadableResponse(resp)
-		fmt.Printf("Header: %s", *header)
-		fmt.Printf("Issues: %v", issues)
-	}
-}
+var responseTests = []struct {
+	function       func(resp *http.Response) (header *string, issues Issues)
+	description    string
+	hstsHeaders    []string
+	expectedIssues Issues
+}{
 
-func TestPreloadableResponseGoodHeader(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
+	/******** PreloadableResponse() ********/
 
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "max-age=10886400; includeSubDomains; preload"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := PreloadableResponse(resp)
-	expectString(t, header, sentHeader)
-	expectIssuesEmpty(t, issues)
-}
-
-func TestPreloadableResponseEmpty(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := ""
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := PreloadableResponse(resp)
-	expectString(t, header, sentHeader)
-	expectIssuesEqual(t, issues,
+	{
+		PreloadableResponse,
+		"good header",
+		[]string{"max-age=10886400; includeSubDomains; preload"},
+		Issues{},
+	},
+	{
+		PreloadableResponse,
+		"missing preload",
+		[]string{"max-age=10886400; includeSubDomains"},
+		Issues{Errors: []string{"Header requirement error: Header must contain the `preload` directive."}},
+	},
+	{
+		PreloadableResponse,
+		"missing includeSubDomains",
+		[]string{"preload; max-age=10886400"},
+		NewIssues().addErrorf("Header requirement error: Header must contain the `includeSubDomains` directive."),
+	},
+	{
+		PreloadableResponse,
+		"single header, multiple errors",
+		[]string{"includeSubDomains; max-age=100"},
+		Issues{
+			Errors: []string{
+				"Header requirement error: Header must contain the `preload` directive.",
+				"Header requirement error: The max-age must be at least 10886400 seconds (== 18 weeks), but the header currently only has max-age=100.",
+			},
+		},
+	},
+	{
+		PreloadableResponse,
+		"empty header",
+		[]string{""},
 		Issues{
 			Errors: []string{
 				"Header requirement error: Header must contain the `includeSubDomains` directive.",
@@ -64,138 +90,74 @@ func TestPreloadableResponseEmpty(t *testing.T) {
 			},
 			Warnings: []string{"Syntax warning: Header is empty."},
 		},
-	)
-}
+	},
+	{
+		PreloadableResponse,
+		"missing header",
+		[]string{},
+		Issues{Errors: []string{"Response error: No HSTS header is present on the response."}},
+	},
+	{
+		PreloadableResponse,
+		"multiple headers",
+		[]string{"max-age=10", "max-age=20", "max-age=30"},
+		Issues{Errors: []string{"Response error: Multiple HSTS headers (number of HSTS headers: 3)."}},
+	},
 
-func TestPreloadableResponseMultipleErrors(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
+	/******** RemovableResponse() ********/
 
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "includeSubDomains; max-age=100"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := PreloadableResponse(resp)
-
-	expectString(t, header, sentHeader)
-	expectIssuesEqual(t, issues,
-		Issues{
-			Errors: []string{
-				"Header requirement error: Header must contain the `preload` directive.",
-				"Header requirement error: The max-age must be at least 10886400 seconds (== 18 weeks), but the header currently only has max-age=100.",
-			},
-			Warnings: []string{},
-		},
-	)
-}
-
-func TestPreloadableResponseMissingIncludeSubDomains(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "preload; max-age=10886400"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := PreloadableResponse(resp)
-
-	expectString(t, header, sentHeader)
-	expectIssuesEqual(t, issues,
-		NewIssues().addErrorf("Header requirement error: Header must contain the `includeSubDomains` directive."),
-	)
-}
-
-func TestPreloadableResponseWithoutHSTSHeaders(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	header, issues := PreloadableResponse(resp)
-	expectNil(t, header)
-
-	expectIssuesEqual(t, issues,
-		NewIssues().addErrorf("Response error: No HSTS header is present on the response."),
-	)
-}
-
-func TestPreloadableResponseMultipleHSTSHeaders(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	resp.Header.Add(key, "max-age=10")
-	resp.Header.Add(key, "max-age=20")
-
-	header, issues := PreloadableResponse(resp)
-	expectNil(t, header)
-
-	expectIssuesEqual(t, issues,
-		NewIssues().addErrorf("Response error: Multiple HSTS headers (number of HSTS headers: 2)."),
-	)
-}
-
-func TestRemovableResponseNoHeader(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	header, issues := RemovableResponse(resp)
-	expectNil(t, header)
-
-	expectIssuesEqual(t, issues,
-		NewIssues().addErrorf("Response error: No HSTS header is present on the response."),
-	)
-}
-
-func TestRemovableResponseNoPreload(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "max-age=15768000; includeSubDomains"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := RemovableResponse(resp)
-	expectString(t, header, sentHeader)
-
-	expectIssuesEmpty(t, issues)
-}
-
-func TestRemovableResponsePreload(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "max-age=15768000; includeSubDomains; preload"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := RemovableResponse(resp)
-	expectString(t, header, sentHeader)
-
-	expectIssuesEqual(t, issues,
-		Issues{
-			Errors:   []string{"Header requirement error: For preload list removal, the header must not contain the `preload` directive."},
-			Warnings: []string{},
-		},
-	)
-}
-
-func TestRemovableResponsePreloadOnly(t *testing.T) {
-	resp := &http.Response{}
-	resp.Header = http.Header{}
-
-	key := http.CanonicalHeaderKey("Strict-Transport-Security")
-	sentHeader := "preload"
-	resp.Header.Add(key, sentHeader)
-
-	header, issues := RemovableResponse(resp)
-	expectString(t, header, sentHeader)
-
-	expectIssuesEqual(t, issues,
+	{
+		RemovableResponse,
+		"no preload",
+		[]string{"max-age=15768000; includeSubDomains"},
+		Issues{},
+	},
+	{
+		RemovableResponse,
+		"preload present",
+		[]string{"max-age=15768000; includeSubDomains; preload"},
+		Issues{Errors: []string{"Header requirement error: For preload list removal, the header must not contain the `preload` directive."}},
+	},
+	{
+		RemovableResponse,
+		"preload only",
+		[]string{"preload"},
 		Issues{
 			Errors: []string{
 				"Header requirement error: For preload list removal, the header must not contain the `preload` directive.",
 				"Header requirement error: Header must contain a valid `max-age` directive.",
 			},
-			Warnings: []string{},
 		},
-	)
+	},
+}
+
+func TestPreloabableResponseAndRemovableResponse(t *testing.T) {
+	for _, tt := range responseTests {
+
+		resp := &http.Response{}
+		resp.Header = http.Header{}
+
+		key := http.CanonicalHeaderKey("Strict-Transport-Security")
+		for _, h := range tt.hstsHeaders {
+			resp.Header.Add(key, h)
+		}
+
+		header, issues := tt.function(resp)
+
+		if len(tt.hstsHeaders) == 1 {
+			if header == nil {
+				t.Errorf("[%s] Did not receive exactly one HSTS header", tt.description)
+			} else if *header != tt.hstsHeaders[0] {
+				t.Errorf("[%s] "+headerStringsShouldBeEqual, tt.description, *header, tt.hstsHeaders[0])
+			}
+		} else {
+			if header != nil {
+				t.Errorf("[%s] Did not expect a header, but received `%s`", tt.description, *header)
+			}
+		}
+
+		if !issuesEqual(issues, tt.expectedIssues) {
+			t.Errorf("[%s] "+issuesShouldBeEqual, tt.description, issues, tt.expectedIssues)
+		}
+	}
 }

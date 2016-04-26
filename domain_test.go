5b9bc772947d9aa68d92968645694b10c679db3b
Lucas Garron <lgarron@chromium.org>
2016-04-26 00:30:48 -0700

    Rewrite PreloadableDomain() tests to use a table-driven test.

--- domain_test.go
+++ domain_test.go
@@ -77,73 +77,91 @@ func skipIfShort(t *testing.T) {
 	}
 }
 
-func TestPreloadableDomainIncompleteChain(t *testing.T) {
-	skipIfShort(t)
-	header, issues := PreloadableDomain("incomplete-chain.badssl.com")
-	expectNil(t, header)
+var preloadableDomainTests = []struct {
+	description    string
+	domain         string
+	expectHeader   bool
+	expectedHeader string
+	expectedIssues Issues
+}{
 
-	expected := Issues{
-		Errors: []string{
-			"Domain error: `incomplete-chain.badssl.com` is a subdomain. Please preload `badssl.com` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics.",
-			"TLS Error: We cannot connect to https://incomplete-chain.badssl.com using TLS (\"Get https://incomplete-chain.badssl.com: x509: certificate signed by unknown authority\"). This might be caused by an incomplete certificate chain, which causes issues on mobile devices. Check out your site at https://www.ssllabs.com/ssltest/",
+	/********* preloadable ********/
+
+	{
+		"valid HSTS",
+		"wikipedia.org",
+		true, "max-age=31536000; includeSubDomains; preload",
+		Issues{},
+	},
+
+	/********* not preloadable ********/
+
+	{
+		"incomplete chain",
+		"incomplete-chain.badssl.com",
+		false, "",
+		Issues{
+			Errors: []string{
+				"Domain error: `incomplete-chain.badssl.com` is a subdomain. Please preload `badssl.com` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics.",
+				"TLS Error: We cannot connect to https://incomplete-chain.badssl.com using TLS (\"Get https://incomplete-chain.badssl.com: x509: certificate signed by unknown authority\"). This might be caused by an incomplete certificate chain, which causes issues on mobile devices. Check out your site at https://www.ssllabs.com/ssltest/",
+			},
 		},
-	}
-	if !issuesEqual(issues, expected) {
-		t.Errorf(issuesShouldBeEqual, issues, expected)
-	}
-}
-
-func TestPreloadableDomainSHA1(t *testing.T) {
-	skipIfShort(t)
-	header, issues := PreloadableDomain("sha1.badssl.com")
-	expectNil(t, header)
-
-	expected := Issues{
-		Errors: []string{
-			"Domain error: `sha1.badssl.com` is a subdomain. Please preload `badssl.com` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics.",
-			"TLS error: One or more of the certificates in your certificate chain is signed using SHA-1. This needs to be replaced. See https://security.googleblog.com/2015/12/an-update-on-sha-1-certificates-in.html. (The first SHA-1 certificate found has a common-name of \"*.badssl.com\".)",
-			"Response error: No HSTS header is present on the response.",
+	},
+	{
+		"SHA-1",
+		"sha1.badssl.com",
+		false, "",
+		Issues{
+			Errors: []string{
+				"Domain error: `sha1.badssl.com` is a subdomain. Please preload `badssl.com` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics.",
+				"TLS error: One or more of the certificates in your certificate chain is signed using SHA-1. This needs to be replaced. See https://security.googleblog.com/2015/12/an-update-on-sha-1-certificates-in.html. (The first SHA-1 certificate found has a common-name of \"*.badssl.com\".)",
+				"Response error: No HSTS header is present on the response.",
+			},
 		},
-	}
-	if !issuesEqual(issues, expected) {
-		t.Errorf(issuesShouldBeEqual, issues, expected)
-	}
-}
-
-func TestPreloadableDomainWithValidHSTS(t *testing.T) {
-	skipIfShort(t)
-	header, issues := PreloadableDomain("wikipedia.org")
-	expectString(t, header, "max-age=31536000; includeSubDomains; preload")
-
-	if !issuesEmpty(issues) {
-		t.Errorf(issuesShouldBeEmpty, issues)
-	}
-}
-
-func TestPreloadableDomainSubdomain(t *testing.T) {
-	skipIfShort(t)
-	header, issues := PreloadableDomain("en.wikipedia.org")
-	expectString(t, header, "max-age=31536000; includeSubDomains; preload")
-
-	expected := NewIssues().addErrorf("Domain error: `en.wikipedia.org` is a subdomain. Please preload `wikipedia.org` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics.")
-	if !issuesEqual(issues, expected) {
-		t.Errorf(issuesShouldBeEqual, issues, expected)
-	}
-}
-
-func TestPreloadableDomainWithoutHSTS(t *testing.T) {
-	skipIfShort(t)
-	header, issues := PreloadableDomain("example.com")
-	expectNil(t, header)
-
-	expected := Issues{
-		Errors: []string{
-			"Response error: No HSTS header is present on the response.",
-			"Redirect error: `http://example.com` does not redirect to `https://example.com`.",
+	},
+	{
+		"subdomain",
+		"en.wikipedia.org",
+		true, "max-age=31536000; includeSubDomains; preload",
+		NewIssues().addErrorf("Domain error: `en.wikipedia.org` is a subdomain. Please preload `wikipedia.org` instead. The interaction of cookies, HSTS and user behaviour is complex; we believe that only accepting whole domains is simple enough to have clear security semantics."),
+	},
+	{
+		"no HSTS",
+		"example.com",
+		false, "",
+		Issues{
+			Errors: []string{
+				"Response error: No HSTS header is present on the response.",
+				"Redirect error: `http://example.com` does not redirect to `https://example.com`.",
+			},
 		},
-	}
-	if !issuesEqual(issues, expected) {
-		t.Errorf(issuesShouldBeEqual, issues, expected)
+	},
+	// Don't run this test like normal. See TestPreloadableDomainBogusDomain().
+	// {
+	// 	"bogus domain",
+	// 	"example.notadomain",
+	// 	false, "",
+	// 	NewIssues().addErrorf("TLS Error: We cannot connect to https://example.notadomain using TLS (\"Get https://example.notadomain: dial tcp: lookup example.notadomain: no such host\"). This might be caused by an incomplete certificate chain, which causes issues on mobile devices. Check out your site at https://www.ssllabs.com/ssltest/"),
+	// },
+}
+
+func TestPreloadableDomain(t *testing.T) {
+	skipIfShort(t)
+
+	for _, tt := range preloadableDomainTests {
+		header, issues := PreloadableDomain(tt.domain)
+
+		if tt.expectHeader {
+			if header == nil {
+				t.Errorf("[%s] %s: Did not receive exactly one HSTS header", tt.description, tt.domain)
+			} else if *header != tt.expectedHeader {
+				t.Errorf("[%s] %s: "+headersStringsShouldBeEqual, tt.description, tt.domain, header, tt.expectedHeader)
+			}
+		}
+
+		if !issuesEqual(issues, tt.expectedIssues) {
+			t.Errorf("[%s] %s: "+issuesShouldBeEqual, tt.description, tt.domain, issues, tt.expectedIssues)
+		}
 	}
 }
 
@@ -163,14 +181,6 @@ func TestPreloadableDomainBogusDomain(t *testing.T) {
 	if !strings.HasSuffix(issues.Errors[0], "no such host\"). This might be caused by an incomplete certificate chain, which causes issues on mobile devices. Check out your site at https://www.ssllabs.com/ssltest/") {
 		t.Errorf("Expected one issues.")
 	}
-
-	// Normal test
-	// header, issues := PreloadableDomain("example.notadomain")
-	// expectNil(t, header)
-	// expected := NewIssues().addErrorf("TLS Error: We cannot connect to https://example.notadomain using TLS (\"Get https://example.notadomain: dial tcp: lookup example.notadomain: no such host\"). This might be caused by an incomplete certificate chain, which causes issues on mobile devices. Check out your site at https://www.ssllabs.com/ssltest/")
-	// if !issuesEqual(issues, expected) {
-	// 	t.Errorf(issuesShouldBeEqual, issues, expected)
-	// }
 }
 
 func chainEquals(actual []*url.URL, expected []string) bool {

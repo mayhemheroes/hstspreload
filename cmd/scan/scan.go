cc1d736ebfe9fa32b05f6a48ef8901354b3dfd4e
Lucas Garron <lgarron@chromium.org>
2016-04-29 12:48:10 -0700

    Parallelism!

--- cmd/scan/scan.go
+++ cmd/scan/scan.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"sync"
 
 	"github.com/chromium/hstspreload"
 )
@@ -20,29 +22,64 @@ type Result struct {
 }
 
 func Scan(domains []string) (results []Result) {
-	c := make(chan Result)
+	in := make(chan string)
+	out := make(chan Result)
 
-	parallel := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(parallelism + 1)
 	for i := 0; i < parallelism; i++ {
-		parallel <- i
+		go func(i int) {
+			for true {
+				d := <-in
+				if d == "" {
+					break
+				}
+				fmt.Printf("[%d] %s...\n", i, d)
+				header, issues := hstspreload.PreloadableDomain(d)
+
+				// Retry once.
+				if len(issues.Errors) != 0 {
+					fmt.Printf("[%d] retrying %s\n", i, d)
+					header, issues = hstspreload.PreloadableDomain(d)
+				}
+
+				fmt.Printf("[%d] ✅  %s\n", i, d)
+
+				r := Result{d, header, issues}
+
+				j, err := json.MarshalIndent(r, "", "  ")
+				if err != nil {
+					fmt.Printf("[%d] %s ❌ json %s \n", i, d, err)
+				} else {
+					err = ioutil.WriteFile("domains/"+d+".json", j, 0644)
+					if err != nil {
+						fmt.Printf("[%d] %s ❌ write %s \n", i, d, err)
+					}
+				}
+
+				out <- r
+			}
+			fmt.Printf("[%d] done\n", i)
+			wg.Done()
+		}(i)
 	}
 
-	// TODO: parallelism semaphore
-	for _, domain := range domains {
-		go func(d string) {
-			fmt.Printf("%s...\n", d)
-			i := <-parallel
-			fmt.Printf("[%d] %s...\n", i, d)
-			header, issues := hstspreload.PreloadableDomain(d)
-			fmt.Printf("[%d] ✅  %s\n", i, d)
-			c <- Result{d, header, issues}
-			parallel <- i
-		}(domain)
+	go func() {
+		for _, _ = range domains {
+			results = append(results, <-out)
+		}
+		wg.Done()
+	}()
+
+	for _, d := range domains {
+		in <- d
+	}
+	for i := 0; i < parallelism; i++ {
+		in <- ""
 	}
 
-	for _, _ = range domains {
-		results = append(results, <-c)
-	}
+	wg.Wait()
+
 	return results
 }
 
@@ -64,4 +101,5 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Printf("%s", j)
+	err = ioutil.WriteFile("output.json", j, 0644)
 }

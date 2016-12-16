2b040d47a99ff321e39e88ca16232499405594db
Lucas Garron <lgarron@chromium.org>
2016-12-15 18:46:58 -0800

    Remove transport_security_state_static_generate
    As a sanity check, I've verified that the new C++ implementation in the Chromium source (https://codereview.chromium.org/2551153003) matches the Go output for the latest 32 revisions of the Chromium repo.

--- doc.go
+++ doc.go
@@ -5,6 +5,4 @@
 // - The `chromiumpreload` package, to query Chromium preload list state.
 //
 // - The `hstspreload` command line tool.
-//
-// - The `transport_security_state_static_generate` script.
 package hstspreload

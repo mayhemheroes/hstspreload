5afa60fca5b4646615e15d10a3d0dcde341b16e8
Lucas Garron <lgarron@chromium.org>
2017-05-23 12:47:57 -0700

    Move checkChain and checkCipherSuite into a file for TLS.

--- domain.go
+++ domain.go
@@ -2,7 +2,6 @@ package hstspreload
 
 import (
 	"crypto/tls"
-	"crypto/x509"
 	"net"
 	"net/http"
 	"strings"
@@ -68,7 +67,7 @@ func PreloadableDomain(domain string) (header *string, issues Issues) {
 	issues = combineIssues(issues, respIssues)
 	if len(respIssues.Errors) == 0 {
 		issues = combineIssues(issues, checkChain(*resp.TLS))
-		issues = combineIssues(issues, checkCipher(*resp.TLS))
+		issues = combineIssues(issues, checkCipherSuite(*resp.TLS))
 
 		preloadableResponse := make(chan Issues)
 		httpRedirectsGeneral := make(chan Issues)
@@ -250,60 +249,6 @@ func preloadableDomainLevel(domain string) Issues {
 	}
 
 	return issues
-}
-
-func checkChain(connState tls.ConnectionState) Issues {
-	fullChain := connState.VerifiedChains[0]
-	chain := fullChain[:len(fullChain)-1] // Ignore the root CA
-	return checkSHA1(chain)
-}
-
-func checkSHA1(chain []*x509.Certificate) Issues {
-	issues := Issues{}
-
-	for _, cert := range chain {
-		if cert.SignatureAlgorithm == x509.SHA1WithRSA || cert.SignatureAlgorithm == x509.ECDSAWithSHA1 {
-			return issues.addErrorf(
-				IssueCode("domain.tls.sha1"),
-				"SHA-1 Certificate",
-				"One or more of the certificates in your certificate chain "+
-					"is signed using SHA-1. This needs to be replaced. "+
-					"See https://security.googleblog.com/2015/12/an-update-on-sha-1-certificates-in.html. "+
-					"(The first SHA-1 certificate found has a common-name of %q.)",
-				cert.Subject.CommonName,
-			)
-		}
-	}
-
-	return issues
-}
-
-func checkCipher(connState tls.ConnectionState) Issues {
-	issues := Issues{}
-
-	// No need to check the TLS version, since the modern ciphers are only supported on TLS 1.2
-
-	switch connState.CipherSuite {
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
-		fallthrough
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
-		fallthrough
-	case tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:
-		fallthrough
-	case tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
-		fallthrough
-	case tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
-		fallthrough
-	case tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:
-		return Issues{}
-	default:
-		return issues.addWarningf(
-			IssueCode("tls.obsolete_cipher_suite"),
-			"Obsolete Cipher Suite",
-			"We could not connect to your site using a modern, secure cipher suite. "+
-				"Check out your site at https://www.ssllabs.com/ssltest/",
-		)
-	}
 }
 
 func checkWWW(host string) Issues {
---
+++ tls.go
@@ -0,0 +1,60 @@
+package hstspreload
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+)
+
+func checkChain(connState tls.ConnectionState) Issues {
+	fullChain := connState.VerifiedChains[0]
+	chain := fullChain[:len(fullChain)-1] // Ignore the root CA
+	return checkSHA1(chain)
+}
+
+func checkSHA1(chain []*x509.Certificate) Issues {
+	issues := Issues{}
+
+	for _, cert := range chain {
+		if cert.SignatureAlgorithm == x509.SHA1WithRSA || cert.SignatureAlgorithm == x509.ECDSAWithSHA1 {
+			return issues.addErrorf(
+				IssueCode("domain.tls.sha1"),
+				"SHA-1 Certificate",
+				"One or more of the certificates in your certificate chain "+
+					"is signed using SHA-1. This needs to be replaced. "+
+					"See https://security.googleblog.com/2015/12/an-update-on-sha-1-certificates-in.html. "+
+					"(The first SHA-1 certificate found has a common-name of %q.)",
+				cert.Subject.CommonName,
+			)
+		}
+	}
+
+	return issues
+}
+
+func checkCipherSuite(connState tls.ConnectionState) Issues {
+	issues := Issues{}
+
+	// No need to check the TLS version, since the modern ciphers are only supported on TLS 1.2
+
+	switch connState.CipherSuite {
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
+		fallthrough
+	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
+		fallthrough
+	case tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:
+		fallthrough
+	case tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
+		fallthrough
+	case tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+		fallthrough
+	case tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:
+		return Issues{}
+	default:
+		return issues.addWarningf(
+			IssueCode("tls.obsolete_cipher_suite"),
+			"Obsolete Cipher Suite",
+			"We could not connect to your site using a modern, secure cipher suite. "+
+				"Check out your site at https://www.ssllabs.com/ssltest/",
+		)
+	}
+}
